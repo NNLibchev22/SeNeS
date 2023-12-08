da209eb8eaa81778fba27495587508d6c84bae36
Simeon Stefanov <SSStefanov22@codingburgas.bg>
2023-12-08 18:50:16 +0200

    js fixed and flip cards animations done

--- JS Files/main.js
+++ JS Files/main.js
@@ -1,14 +1,32 @@
-const initSlider = () => {
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loads");
+
     const imageList = document.querySelector(".bio-items");
-    const slideButtons = document.querySelectorAll(".bio-items #buttons");
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
 
-    slideButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const direction = button.id === "prev" ? -1 : 1;
-            const scrollAmount = imageList.clientWidth * direction;
-            imageList.scrollBy({ left: scrollAmount, behavior: "smooth"})
-        })
-    })
-}
+    let currentPosition = 0;
+    const step = 25; 
+    const maxLeftPosition = 0; 
+    const maxRightPosition = -25;
 
-window.addEventListener("load", initSlider)
+    prevButton.addEventListener("click", () => {
+        console.log("loads - prev button clicked");
+        if (currentPosition < maxLeftPosition) {
+            currentPosition += step;
+            currentPosition = Math.min(currentPosition, maxLeftPosition);
+            imageList.style.transition = "transform 0.3s ease-in-out";
+            imageList.style.transform = `translateX(${currentPosition}%)`;
+        }
+    });
+
+    nextButton.addEventListener("click", () => {
+        console.log("loads - next button clicked");
+        if (currentPosition > maxRightPosition) {
+            currentPosition -= step;
+            currentPosition = Math.max(currentPosition, maxRightPosition);
+            imageList.style.transition = "transform 0.3s ease-in-out";
+            imageList.style.transform = `translateX(${currentPosition}%)`;
+        }
+    });
+});
