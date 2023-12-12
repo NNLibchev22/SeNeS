2f882d6da075195d16e3a43e0f24f2a434925f00
Simeon Stefanov <SSStefanov22@codingburgas.bg>
2023-12-12 21:28:40 +0200

    navbar fix

---
+++ JS Files/contact.js
@@ -0,0 +1,23 @@
+const header = document.querySelector('header');
+const stop = document.querySelector('.starting-img')
+
+const sectionOneOptions = {
+    rootMargin: " -100px 0px 0px 0px"
+};
+
+const stopOneObserver = new IntersectionObserver
+(function(
+    entries,
+    stopOneObserver
+){
+    entries.forEach(entry => {
+    if(!entry.isIntersecting){
+        header.classList.add('nav-scrolled');
+    }else{
+        header.classList.remove('nav-scrolled');
+    }
+    });
+},
+sectionOneOptions);
+
+stopOneObserver.observe(stop);
